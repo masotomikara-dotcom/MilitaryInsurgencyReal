8574dde148942451ca15ac3ee073b2d9a59e92c6
masotomikara-dotcom <masotomikara@gmail.com>
2026-04-27 19:22:24 +0700

    syntax fix.

--- src/main/java/com/example/mod/client/renderer/CivilNVGRenderer.java
+++ src/main/java/com/example/mod/client/renderer/CivilNVGRenderer.java
@@ -2,7 +2,7 @@ package com.example.mod.client.renderer;
 
 import com.example.mod.client.model.CivilNVGModel;
 import com.example.mod.item.CivilNVGItem;
-import software.bernie.geckolib.renderer.layer.GeoArmorRenderer;
+import software.bernie.geckolib.renderer.GeoArmorRenderer; // Fixed import path
 
 public class CivilNVGRenderer extends GeoArmorRenderer<CivilNVGItem> {
     public CivilNVGRenderer() {
