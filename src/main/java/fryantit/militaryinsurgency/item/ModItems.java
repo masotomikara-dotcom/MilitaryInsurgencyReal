db689834c4a3f0a201f3df3c861dd729d85c51c8
masotomikara-dotcom <masotomikara@gmail.com>
2026-04-30 19:21:00 +0700

    delete Armor configuration.

--- src/main/java/fryantit/militaryinsurgency/item/ModItems.java
+++ src/main/java/fryantit/militaryinsurgency/item/ModItems.java
@@ -1,17 +1,20 @@
 package fryantit.militaryinsurgency.item;
 
-import fryantit.militaryinsurgency.armor.NVGArmorItem;
-import net.minecraft.item.ArmorMaterials;
+import fryantit.militaryinsurgency.MilitaryInsurgency;
+import fryantit.militaryinsurgency.item.custom.NVGItem;
 import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
 public class ModItems {
-    // Registry for the NVG Item
-    public static final Item NVG_ITEM = new NVGItem(ArmorMaterials.NETHERITE, NVGItem.Type.HELMET, new Item.Settings().maxCount(1));
+    public static final Item NVG_ITEM = registerItem("nvg_item", new NVGItem(new Item.Settings().maxCount(1)));
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, new Identifier(MilitaryInsurgency.MOD_ID, name), item);
+    }
 
     public static void registerModItems() {
-        Registry.register(Registries.ITEM, new Identifier("militaryinsurgency", "civil_nvg"), NVG_ITEM);
+        MilitaryInsurgency.LOGGER.info("Registering Mod Items for " + MilitaryInsurgency.MOD_ID);
     }
 }
