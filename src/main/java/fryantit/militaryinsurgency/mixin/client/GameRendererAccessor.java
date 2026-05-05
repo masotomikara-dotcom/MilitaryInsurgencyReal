a5f16d063f916b7e342f44394522f98486ee766e
masotomikara-dotcom <masotomikara@gmail.com>
2026-05-05 10:15:39 +0700

    debug

---
+++ src/main/java/fryantit/militaryinsurgency/mixin/client/GameRendererAccessor.java
@@ -0,0 +1,12 @@
+package fryantit.militaryinsurgency.mixin.client;
+
+import net.minecraft.client.render.GameRenderer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Invoker;
+import net.minecraft.util.Identifier;
+
+@Mixin(GameRenderer.class)
+public interface GameRendererAccessor {
+    @Invoker("loadPostProcessor")
+    void invokeLoadPostProcessor(Identifier identifier);
+}
